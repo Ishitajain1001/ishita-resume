c7626adc2e19cd0446492956d487b576f5bae6e9
Ishita Jain <ishitajain1001@gmail.com>
2020-11-19 23:38:05 -0600

    setting up server attempt

--- database.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  //Initialize Firebase
-  const config = {
-    apiKey: "AIzaSyCqEUzfLiv3h49mgGPvFF9Ppeoq92R8cSQ",
-    authDomain: "https://resume-website-77345.firebaseapp.com/"
-  }
-}());
